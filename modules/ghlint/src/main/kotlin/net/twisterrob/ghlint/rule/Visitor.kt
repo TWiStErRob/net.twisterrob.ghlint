9da7a3fd0d9d1bb020ebfa94893a7014bad0bdbf
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-09 22:32:27 +0000

    Add JSR annotations

--- modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/rule/Visitor.kt
+++ modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/rule/Visitor.kt
@@ -3,15 +3,18 @@ package net.twisterrob.ghlint.rule
 import net.twisterrob.ghlint.model.Job
 import net.twisterrob.ghlint.model.Step
 import net.twisterrob.ghlint.model.Workflow
+import javax.annotation.OverridingMethodsMustInvokeSuper
 
 public interface Visitor {
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitWorkflow(reporting: Reporting, workflow: Workflow) {
 		workflow.jobs.values.forEach { job ->
 			visitJob(reporting, job)
 		}
 	}
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitJob(reporting: Reporting, job: Job) {
 		when (job) {
 			is Job.NormalJob -> visitNormalJob(reporting, job)
@@ -19,16 +22,19 @@ public interface Visitor {
 		}
 	}
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitNormalJob(reporting: Reporting, job: Job.NormalJob) {
 		job.steps.forEach { step ->
 			visitStep(reporting, step)
 		}
 	}
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitReusableWorkflowCallJob(reporting: Reporting, job: Job.ReusableWorkflowCallJob) {
 		// No children.
 	}
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitStep(reporting: Reporting, step: Step) {
 		when (step) {
 			is Step.Run -> visitRunStep(reporting, step)
@@ -36,10 +42,12 @@ public interface Visitor {
 		}
 	}
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitUsesStep(reporting: Reporting, step: Step.Uses) {
 		// No children.
 	}
 
+	@OverridingMethodsMustInvokeSuper
 	public fun visitRunStep(reporting: Reporting, step: Step.Run) {
 		// No children.
 	}
