5580969dcaf6f6fc6d3e4a85cc08e36c0c192ffe
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-09 23:03:53 +0000

    Enable new rules

--- modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/analysis/Main.kt
+++ modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/analysis/Main.kt
@@ -4,17 +4,27 @@ import net.twisterrob.ghlint.model.File
 import net.twisterrob.ghlint.model.FileName
 import net.twisterrob.ghlint.reporting.SarifReporter
 import net.twisterrob.ghlint.reporting.TextReporter
+import net.twisterrob.ghlint.rules.CreatePullRequestRule
 import net.twisterrob.ghlint.rules.IdNamingRule
 import net.twisterrob.ghlint.rules.MandatoryNameRule
 import net.twisterrob.ghlint.rules.MandatoryShellRule
+import net.twisterrob.ghlint.rules.RemoveEmptyEnvRule
+import net.twisterrob.ghlint.rules.SetDefaultShellRule
+import net.twisterrob.ghlint.rules.UploadArtifactShouldFailOnMissingFilesRule
+import net.twisterrob.ghlint.rules.UseGhTokenWithGhCliRule
 import java.nio.file.Path
 import kotlin.io.path.bufferedWriter
 
 public fun main(vararg args: String) {
 	val rules = listOf(
-		MandatoryNameRule(),
+		CreatePullRequestRule(),
 		IdNamingRule(),
+		MandatoryNameRule(),
 		MandatoryShellRule(),
+		RemoveEmptyEnvRule(),
+		SetDefaultShellRule(),
+		UploadArtifactShouldFailOnMissingFilesRule(),
+		UseGhTokenWithGhCliRule(),
 	)
 	val files = args.map { File(FileName(it)) }
 
