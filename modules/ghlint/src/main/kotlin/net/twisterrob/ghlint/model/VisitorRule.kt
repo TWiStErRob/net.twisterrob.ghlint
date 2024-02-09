85b82c123e3c7890a872fc31eb34814a84372279
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-09 09:44:13 +0000

    Remove unused code

--- modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/model/VisitorRule.kt
+++ modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/model/VisitorRule.kt
@@ -5,19 +5,8 @@ public interface VisitorRule : Rule, Visitor {
 	override fun check(workflow: Workflow): List<Finding> {
 		val reporting = object : Reporting {
 			val findings: MutableList<Finding> = mutableListOf()
-			private val state: MutableMap<Rule, MutableMap<String, Any?>> = mutableMapOf()
 			override fun report(issue: Issue, context: Model) {
 				findings.add(issue.problem(context))
-			}
-
-			override fun putState(rule: Rule, key: String, value: Any?) {
-				val ruleState = state.getOrPut(rule) { mutableMapOf() }
-				ruleState[key] = value
-			}
-
-			override fun getState(rule: Rule, key: String): Any? {
-				val ruleState = state.getOrPut(rule) { mutableMapOf() }
-				return ruleState[key]
 			}
 		}
 		visitWorkflow(reporting, workflow)
@@ -32,11 +21,13 @@ public interface Visitor {
 			visitJob(reporting, job)
 		}
 	}
+
 	public fun visitJob(reporting: Reporting, job: Job) {
 		job.steps.forEach { step ->
 			visitStep(reporting, step)
 		}
 	}
+
 	public fun visitStep(reporting: Reporting, step: Step) {
 		// No children.
 	}
@@ -45,7 +36,4 @@ public interface Visitor {
 public interface Reporting {
 
 	public fun report(issue: Issue, context: Model)
-
-	public fun putState(rule: Rule, key: String, value: Any?)
-	public fun getState(rule: Rule, key: String): Any?
 }
