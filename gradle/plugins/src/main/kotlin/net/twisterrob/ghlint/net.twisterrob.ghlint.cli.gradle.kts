ad3cb3f6b2c0f49a44bf90ad2472bf5b3400fc09
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-12 09:39:25 +0000

    Fat JAR

--- gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/net.twisterrob.ghlint.cli.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/net.twisterrob.ghlint.cli.gradle.kts
@@ -13,3 +13,28 @@ kotlin {
 }
 
 tasks.named<JavaExec>("run").configure { setWorkingDir(rootProject.layout.projectDirectory) }
+
+tasks.register<Jar>("fatJar") {
+	manifest {
+		attributes(
+			"Main-Class" to application.mainClass.get()
+		)
+	}
+	archiveClassifier = "fat"
+	dependsOn(configurations.runtimeClasspath)
+	from(configurations.runtimeClasspath.map { it.map(::zipTree) })
+	with(tasks.jar.get())
+	exclude(
+		"META-INF/versions/9/module-info.class"
+	)
+	duplicatesStrategy = DuplicatesStrategy.FAIL
+
+	doLast {
+		// TODO This makes the file polyglot and `./foo-fat.jar`-executable
+		//val file = archiveFile.get().asFile
+		//val bytes = file.readBytes()
+		//file.writeText("#!/usr/bin/env java -jar")
+		//file.appendBytes(bytes)
+		//file.setExecutable(true)
+	}
+}
