da2a67463e4c555a750b7281e018f8bf3d08834a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-12 18:46:35 +0000

    Enable tests

--- modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/DefaultRuleSetTest.kt
+++ modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/DefaultRuleSetTest.kt
@@ -1,9 +1,9 @@
 package net.twisterrob.ghlint.rules
 
-//import net.twisterrob.ghlint.testing.validate
-//import org.junit.jupiter.api.TestFactory
+import net.twisterrob.ghlint.testing.validate
+import org.junit.jupiter.api.TestFactory
 
 class DefaultRuleSetTest {
 
-//	@TestFactory fun test() = validate(DefaultRuleSet::class)
+	@TestFactory fun test() = validate(DefaultRuleSet::class)
 }
