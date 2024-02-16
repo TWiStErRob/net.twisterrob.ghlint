38deab9cf6df6880bd54bb564a5abf1b6df8cb0f
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-16 08:59:52 +0000

    DoubleCurlyIfRule

---
+++ modules/ghlint-rules/src/main/kotlin/net/twisterrob/ghlint/rules/DoubleCurlyIfRule.kt
@@ -0,0 +1,174 @@
+package net.twisterrob.ghlint.rules
+
+import net.twisterrob.ghlint.model.Component
+import net.twisterrob.ghlint.model.Job
+import net.twisterrob.ghlint.model.Step
+import net.twisterrob.ghlint.rule.Example
+import net.twisterrob.ghlint.rule.Issue
+import net.twisterrob.ghlint.rule.Reporting
+import net.twisterrob.ghlint.rule.VisitorRule
+import net.twisterrob.ghlint.rule.report
+
+public class DoubleCurlyIfRule : VisitorRule {
+
+	override val issues: List<Issue> = listOf(DoubleCurlyIf)
+
+	override fun visitJob(reporting: Reporting, job: Job) {
+		super.visitJob(reporting, job)
+		val condition = (job.`if` ?: return)
+		validate(reporting, job, condition)
+	}
+
+	override fun visitStep(reporting: Reporting, step: Step) {
+		super.visitStep(reporting, step)
+		val condition = (step.`if` ?: return)
+		validate(reporting, step, condition)
+	}
+
+	private fun validate(reporting: Reporting, target: Component, condition: String) {
+		if (hasDoubleCurlyInside(condition)) {
+			reporting.report(DoubleCurlyIf, target) { "${it} has nested or invalid double-curly-braces." }
+		} else if (!isWrappedInDoubleCurly(condition)) {
+			reporting.report(DoubleCurlyIf, target) { "${it} does not have double-curly-braces." }
+		}
+	}
+
+	internal companion object {
+
+		private fun isWrappedInDoubleCurly(condition: String): Boolean =
+			condition.startsWith("\${{") && condition.endsWith("}}")
+
+		private fun hasDoubleCurlyInside(condition: String): Boolean {
+			val startCurly = condition.indexOf(string = "\${{")
+			val endCurly = condition.indexOf("}}")
+			return startCurly != -1 && startCurly != 0 || endCurly != -1 && endCurly != condition.length - 2
+		}
+
+		val DoubleCurlyIf = Issue(
+			id = "DoubleCurlyIf",
+			title = "if: is not wrapped in double-curly-braces.",
+			description = """
+				Omitting, or over-using the double-curly-braces (`${'$'}{{ }}`) can lead to unexpected behavior.
+				
+				While the [GitHub Actions Expressions documentation](https://docs.github.com/en/actions/learn-github-actions/expressions)
+				has a note for `${'$'}{{ }}` being optional for `if:`s.
+				The `if:` being an exception also has an exception,
+				as [documented on conditionals](https://docs.github.com/en/actions/using-jobs/using-conditions-to-control-job-execution).
+				
+				The optionality listed above probably causes more problems than keystrokes it saves,
+				and therefore it's strongly recommended to always wrap the full `if:` condition in `${'$'}{{ }}`.
+				
+				If you find the examples confusing (I definitely did), the more reason to always use it.
+				Simple rule, consistent outcomes.
+			""".trimIndent(),
+			compliant = listOf(
+				Example(
+					explanation = "`if:`s starting with a `!` must always be wrapped in double-curly-braces.",
+					content = """
+						on: push
+						jobs:
+						  example:
+						    if: ${'$'}{{ ! startsWith(github.ref, 'refs/tags/') }}
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+					""".trimIndent()
+				),
+				Example(
+					explanation = "To avoid confusion, `if:` is fully wrapped in double-curly-braces.",
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        if: ${'$'}{{ steps.calculation.outputs.result == 'world' }}
+					""".trimIndent()
+				),
+			),
+			nonCompliant = listOf(
+				Example(
+					explanation = """
+						When `if:` starts with `!`, it's going to break the condition.
+						A string value starting with `!` is reserved syntax in YAML.
+					""".trimIndent(),
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        if: ! startsWith(github.ref, 'refs/tags/')
+					""".trimIndent()
+				),
+				Example(
+					explanation = """
+						Simple comparison is non-commutative due to YAML syntax.
+						The first step in this example will work correctly,
+						but as soon as the condition order is swapped around the YAML doesn't even parse:
+						```log
+						while parsing a block mapping
+						 in reader, line 3, column 5:
+						        if: 'bbb' == github.context.variable
+						        ^
+						expected <block end>, but found '<scalar>'
+						 in reader, line 3, column 15:
+						        if: 'bbb' == github.context.variable
+						                  ^
+						```
+						This is very confusing as commutativity is one of the basics of boolean math in programming languages.
+						Especially for a simple thing like equality.
+						In case both of these are wrapped in double-curly-braces, the order doesn't matter, and it "just works".
+					""".trimIndent(),
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        if: github.context.variable == 'example'
+						#     - run: echo "Example"
+						#       if: 'example' == github.context.variable
+					""".trimIndent()
+				),
+				Example(
+					explanation = """
+						The `if:` condition is wrapped in double-curly-braces, but only partially.
+						Looking at the expression, it might be interpreted (by humans) as a valid string comparison,
+						because the GitHub Actions Context variable is wrapped in an Expression as expected.
+						
+						However, this condition will **always** evaluate to `true`:
+						The way to interpret this expression is as follows:
+						 * Evaluate `steps.calculation.outputs.result` -> `'hello'`
+						 * Substitute `'hello'` -> `if: hello == 'world'`
+						 * Evaluate `"hello == 'world'"` -> `true`
+						
+						This last step might be surprising, but after substituting the expressions,
+						GitHub Actions just leaves us with a YAML String.
+						That string is then passed to `if`, but it's a non-empty string, which is truthy.
+						
+						To confirm this, you can run a workflow with step debugging turned on, and you'll see this:
+						```
+						Evaluating: (success() && format('{0} == ''world''', steps.calculation.outputs.result))
+						```
+					""".trimIndent(),
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "result=hello" >> "${'$'}{GITHUB_OUTPUT}"
+						        id: calculation
+						      - run: echo "Example"
+						        if: ${'$'}{{ steps.calculation.outputs.result }} == 'world'
+					""".trimIndent()
+				),
+			),
+		)
+	}
+}
+
---
+++ modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/DoubleCurlyIfRuleTest.kt
@@ -0,0 +1,239 @@
+package net.twisterrob.ghlint.rules
+
+import io.kotest.matchers.should
+import net.twisterrob.ghlint.testing.beEmpty
+import net.twisterrob.ghlint.testing.check
+import net.twisterrob.ghlint.testing.haveFinding
+import net.twisterrob.ghlint.testing.test
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class DoubleCurlyIfRuleTest {
+
+	@TestFactory fun metadata() = test(DoubleCurlyIfRule::class)
+
+	@Nested
+	inner class DoubleCurlyIfOnJobTest {
+
+		@Test
+		fun `passes even with lots of spacing`() {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    if:     ${'$'}{{     true     }}    
+					    steps:
+					      - run: echo "Test"
+				""".trimIndent()
+			)
+
+			result should beEmpty()
+		}
+
+		@MethodSource(
+			"net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getValidConditions",
+			"net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getValidButSyntaxErrorConditions"
+		)
+		@ParameterizedTest
+		fun `passes wrapped condition`(condition: String) {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    if: ${'$'}{{ ${condition} }}
+					    steps:
+					      - run: echo "Test"
+				""".trimIndent()
+			)
+
+			result should beEmpty()
+
+			val resultNoSpacing = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    if: ${'$'}{{${condition}}}
+					    steps:
+					      - run: echo "Test"
+				""".trimIndent()
+			)
+
+			resultNoSpacing should beEmpty()
+		}
+
+		@MethodSource("net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getValidConditions")
+		@ParameterizedTest
+		fun `fails not fully wrapped condition`(condition: String) {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    if: ${condition}
+					    steps:
+					      - run: echo "Test"
+				""".trimIndent()
+			)
+
+			result should haveFinding(
+				"DoubleCurlyIf",
+				"Job[test] does not have double-curly-braces."
+			)
+		}
+
+		@MethodSource("net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getInvalidConditions")
+		@ParameterizedTest
+		fun `fails not strangely constructed condition`(condition: String) {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    if: ${condition}
+					    steps:
+					      - run: echo "Test"
+				""".trimIndent()
+			)
+
+			result should haveFinding(
+				"DoubleCurlyIf",
+				"Job[test] has nested or invalid double-curly-braces."
+			)
+		}
+	}
+
+	@Nested
+	inner class DoubleCurlyIfOnStepTest {
+
+		@Test
+		fun `passes even with lots of spacing`() {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    steps:
+					      - run: echo "Test"
+					        if:     ${'$'}{{     true     }}    
+				""".trimIndent()
+			)
+
+			result should beEmpty()
+		}
+		@MethodSource(
+			"net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getValidConditions",
+			"net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getValidButSyntaxErrorConditions"
+		)
+		@ParameterizedTest
+		fun `passes wrapped condition`(condition: String) {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    if: ${'$'}{{ ${condition} }}
+					    steps:
+					      - run: echo "Test"
+				""".trimIndent()
+			)
+
+			result should beEmpty()
+
+			val resultNoSpacing = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    steps:
+					      - run: echo "Test"
+					        if: ${'$'}{{${condition}}}
+				""".trimIndent()
+			)
+
+			resultNoSpacing should beEmpty()
+		}
+
+		@MethodSource("net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getValidConditions")
+		@ParameterizedTest
+		fun `fails not fully wrapped condition`(condition: String) {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    steps:
+					      - run: echo "Test"
+					        if: ${condition}
+				""".trimIndent()
+			)
+
+			result should haveFinding(
+				"DoubleCurlyIf",
+				"Step[#0] in Job[test] does not have double-curly-braces."
+			)
+		}
+
+		@MethodSource("net.twisterrob.ghlint.rules.DoubleCurlyIfRuleTest#getInvalidConditions")
+		@ParameterizedTest
+		fun `fails not strangely constructed condition`(condition: String) {
+			val result = check<DoubleCurlyIfRule>(
+				"""
+					jobs:
+					  test:
+					    steps:
+					      - run: echo "Test"
+					        if: ${condition}
+				""".trimIndent()
+			)
+
+			result should haveFinding(
+				"DoubleCurlyIf",
+				"Step[#0] in Job[test] has nested or invalid double-curly-braces."
+			)
+		}
+	}
+
+	@MethodSource("getValidButSyntaxErrorConditions")
+	@ParameterizedTest
+	fun `test validation for syntax error`(condition: String) {
+		val ex = assertThrows<RuntimeException> {
+			check<DoubleCurlyIfRule>(
+				"""
+					if: ${condition}
+				""".trimIndent()
+			)
+		}
+		ex.printStackTrace()
+	}
+
+	companion object {
+
+		@JvmStatic
+		val invalidConditions = listOf(
+			"${'$'}{{ github.context.variable }} == 'test'",
+			"123 == ${'$'}{{ github.context.variable }}",
+			"null == ${'$'}{{ github.context.variable }}",
+			"${'$'}{{ github.context.variable }} == ${'$'}{{ github.other.var }}",
+			"${'$'}{{ github.context.variable }} && ${'$'}{{ github.other.var }} || ${'$'}{{ github.yet.other.var }}",
+		)
+
+		@JvmStatic
+		val validConditions = listOf(
+			"true",
+			"true || false",
+			"null",
+			"github.context.variable == 'bbb'",
+			"github.context.variable == \"bbb\"",
+			"123 == github.context.variable",
+			"true == github.context.variable",
+			"! github.invalid.yaml",
+			"!!github.invalid.yaml",
+		)
+
+		@JvmStatic
+		val validButSyntaxErrorConditions = listOf(
+			"'aaa' == 'bbb'",
+			"\"aaa\" == \"bbb\"",
+			"'aaa' == github.context.variable",
+			"\"aaa\" == github.context.variable",
+		)
+	}
+}
