61dae256fe8bb170d79c3ba445caa0f2693a95e7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-17 17:19:54 +0000

    Split up remaining tests

--- modules/ghlint-testing/src/main/kotlin/net/twisterrob/ghlint/testing/test.kt
+++ modules/ghlint-testing/src/main/kotlin/net/twisterrob/ghlint/testing/test.kt
@@ -1,6 +1,10 @@
 package net.twisterrob.ghlint.testing
 
+import io.kotest.matchers.collections.atLeastSize
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldHave
 import io.kotest.matchers.shouldNot
+import io.kotest.matchers.string.shouldNotStartWith
 import net.twisterrob.ghlint.rule.Issue
 import net.twisterrob.ghlint.rule.Rule
 import net.twisterrob.ghlint.ruleset.RuleSet
@@ -9,6 +13,7 @@ import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import java.util.stream.Stream
 import kotlin.reflect.KClass
+import io.kotest.matchers.string.beEmpty as beEmptyString
 
 /**
  * Validates a [RuleSet], it's [Rule]s and their [Issue]s.
@@ -53,7 +58,7 @@ public fun test(ruleSet: KClass<out RuleSet>): List<DynamicNode> =
  */
 public fun test(rule: KClass<out Rule>): DynamicNode =
 	dynamicContainer(
-		"Rule ${rule.simplerName} is valid",
+		"Rule ${rule.simplerName}",
 		Stream.concat(
 			Stream.of(dynamicTest("Rule ${rule.simplerName} is instantiatable") {
 				rule.java.getDeclaredConstructor().newInstance()
@@ -75,27 +80,79 @@ private fun test(instance: Rule): List<DynamicNode> = listOf(
 		"Rule ${instance::class.simplerName} issues",
 		instance.issues.map { issue ->
 			dynamicContainer(
-				"Issue ${issue.id} is valid",
+				"Issue ${issue.id}",
 				listOf(
-					dynamicTest("Issue ${issue.id} title is valid") {
+					dynamicTest("Issue ${issue.id} title") {
 						validateIssueTitle(issue)
 					},
-					dynamicTest("Issue ${issue.id} description is valid") {
+					dynamicTest("Issue ${issue.id} description") {
 						validateIssueDescription(issue)
 					},
-					// STOPSHIP split into multiple tests
-					dynamicTest("Issue ${issue.id} compliant examples are valid") {
-						instance.validateCompliantExamples(issue)
-					},
-					// STOPSHIP split into multiple tests
-					dynamicTest("Issue ${issue.id} non-compliant examples are valid") {
-						instance.validateNonCompliantExamples(issue)
-					},
+					dynamicContainer(
+						"Issue ${issue.id} compliant examples",
+						testCompliantExamples(instance, issue)
+					),
+					dynamicContainer(
+						"Issue ${issue.id} non-compliant examples",
+						testNonCompliantExamples(instance, issue)
+					),
 				)
 			)
 		}
 	)
 )
+
+private fun testCompliantExamples(instance: Rule, issue: Issue): List<DynamicNode> {
+	val basics = listOf(
+		dynamicTest("Issue ${issue.id} compliant examples are not empty") {
+			issue.compliant shouldHave atLeastSize(1)
+		}
+	)
+	val examples = issue.compliant.mapIndexed { index, example ->
+		val name = "Issue ${issue.id} compliant example #${index + 1}"
+		dynamicContainer(
+			name,
+			listOf(
+				dynamicTest("${name} syntax") {
+					validate(example.content) should beEmpty()
+				},
+				dynamicTest("${name} no findings") {
+					instance.check(example.content) shouldNot haveFinding(issue.id)
+				},
+				dynamicTest("${name} explanation") {
+					example.explanation shouldNot io.kotest.matchers.string.beEmpty()
+					example.explanation shouldNotStartWith "TODO"
+				}
+			)
+		)
+	}
+	return basics + examples
+}
+
+private fun testNonCompliantExamples(instance: Rule, issue: Issue): List<DynamicNode> {
+	val basics = listOf(
+		dynamicTest("Issue ${issue.id} non-compliant examples are not empty") {
+			issue.nonCompliant shouldHave atLeastSize(1)
+		}
+	)
+	val examples = issue.nonCompliant.mapIndexed { index, example ->
+		val name = "Issue ${issue.id} non-compliant example #${index + 1}"
+		dynamicContainer(
+			name,
+			listOf(
+				dynamicTest("${name} findings") {
+					val findings = validate(example.content) + instance.check(example.content)
+					findings should haveFinding(issue.id)
+				},
+				dynamicTest("${name} explanation") {
+					example.explanation shouldNot beEmptyString()
+					example.explanation shouldNotStartWith "TODO"
+				}
+			)
+		)
+	}
+	return basics + examples
+}
 
 private val KClass<*>.simplerName: String
 	get() = this.simpleName ?: this.qualifiedName ?: this.java.name
