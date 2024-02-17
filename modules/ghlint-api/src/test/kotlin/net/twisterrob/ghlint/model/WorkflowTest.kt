e6faf32ae0f6c9bf32dfce9f079e5f9766bae46e
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-17 11:48:09 +0000

    Strip yaml suffix from filename for Workflow id

---
+++ modules/ghlint-api/src/test/kotlin/net/twisterrob/ghlint/model/WorkflowTest.kt
@@ -0,0 +1,45 @@
+package net.twisterrob.ghlint.model
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.ValueSource
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+
+class WorkflowTest {
+
+	@Nested
+	inner class IdTest {
+
+		@ParameterizedTest
+		@ValueSource(
+			strings = [
+				"file-name",
+				"file-name.yml",
+				"file-name.yaml"
+			]
+		)
+		fun `strips extension`(fileName: String) {
+			val wf = mock<Workflow>()
+			whenever(wf.parent).thenReturn(File(FileLocation(fileName), "jobs:"))
+
+			wf.id shouldBe "file-name"
+		}
+
+		@ParameterizedTest
+		@CsvSource(
+			"file-name.yml.yaml, file-name.yml",
+			"file-name.yml.yml, file-name.yml",
+			"file-name.yaml.yml, file-name.yaml",
+			"file-name.yaml.yaml, file-name.yaml",
+		)
+		fun `keeps duplicate extension`(fileName: String, expectedId: String) {
+			val wf = mock<Workflow>()
+			whenever(wf.parent).thenReturn(File(FileLocation(fileName), "jobs:"))
+
+			wf.id shouldBe expectedId
+		}
+	}
+}
