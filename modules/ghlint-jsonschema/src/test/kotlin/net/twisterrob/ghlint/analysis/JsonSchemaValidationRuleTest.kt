22da0851cb03e09578b797c03ebe805a027b6957
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-20 10:07:18 +0000

    Next step

--- modules/ghlint-jsonschema/src/test/kotlin/net/twisterrob/ghlint/analysis/JsonSchemaValidationRuleTest.kt
+++ modules/ghlint-jsonschema/src/test/kotlin/net/twisterrob/ghlint/analysis/JsonSchemaValidationRuleTest.kt
@@ -1,5 +1,8 @@
 package net.twisterrob.ghlint.analysis
 
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import net.twisterrob.ghlint.testing.check
 import net.twisterrob.ghlint.testing.validate
 import org.junit.jupiter.api.Test
 
@@ -7,5 +10,11 @@ class JsonSchemaValidationRuleTest {
 
 	@Test fun metadata() {
 		validate(JsonSchemaValidationRule(), JsonSchemaValidationRule.JsonSchemaValidation)
+	}
+
+	@Test fun `syntax error`() {
+		val findings = check<JsonSchemaValidationRule>("<invalid json />")
+		findings shouldHaveSize 1
+		findings.single().message shouldBe "Invalid JSON"
 	}
 }
