249b8b4c6526178a2d7cc83595fdf464e0aae572
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-20 21:52:39 +0000

    Generate toc and links

--- modules/ghlint-docs/src/main/kotlin/net/twisterrob/ghlint/docs/Generator.kt
+++ modules/ghlint-docs/src/main/kotlin/net/twisterrob/ghlint/docs/Generator.kt
@@ -4,7 +4,7 @@ import net.twisterrob.ghlint.rule.Issue
 import net.twisterrob.ghlint.rule.Rule
 import net.twisterrob.ghlint.ruleset.RuleSet
 import java.nio.file.Path
-import kotlin.io.path.createParentDirectories
+import kotlin.io.path.createDirectories
 import kotlin.io.path.writeText
 
 internal class Generator(
@@ -13,29 +13,55 @@ internal class Generator(
 
 	fun generate(vararg ruleSets: RuleSet) {
 		ruleSets.forEach { ruleSet ->
+			val ruleSetFolder = target.resolve(ruleSet.id).apply { createDirectories() }
+			val ruleSetFile = ruleSetFolder.resolve("index.md")
+			ruleSetFile.writeText(
+				generateDocs(
+					ruleSet = ruleSet
+				)
+			)
 			ruleSet.createRules().forEach { rule ->
 				rule.issues.forEach { issue ->
-					generate(ruleSet, rule, issue)
+					val issueFile = ruleSetFolder.resolve(issue.fileName)
+					issueFile.writeText(
+						generateDocs(
+							ruleSet = ruleSet,
+							rule = rule,
+							issue = issue,
+							relatedIssues = rule.issues - issue
+						)
+					)
 				}
 			}
 		}
 	}
 
-	private fun generate(ruleSet: RuleSet, rule: Rule, issue: Issue) {
-		val docs = generateDocs(ruleSet, rule, issue)
-		val file = target.resolve(ruleSet.id).resolve("${issue.id}.md")
-		file.createParentDirectories()
-		file.writeText(docs)
-	}
-
-	private fun generateDocs(ruleSet: RuleSet, rule: Rule, issue: Issue): String =
+	private fun generateDocs(ruleSet: RuleSet): String =
 		buildString {
+			appendLine(
+				"""
+					# Rule set "${ruleSet.name}" (`${ruleSet.id}`)
+					
+				""".trimIndent()
+			)
+			ruleSet.createRules().sortedBy { it.id }.forEach { rule ->
+				appendLine(" - `${rule.id}`")
+				rule.issues.sortedBy { it.id }.forEach { issue ->
+					appendLine("    - [`${issue.id}`](${issue.fileName}): ${issue.title}")
+				}
+			}
+		}
+
+	private fun generateDocs(ruleSet: RuleSet, rule: Rule, issue: Issue, relatedIssues: List<Issue>): String =
+		buildString {
+			val relatedIssuesText = relatedIssues.joinToString(separator = ", ") { "[`${it.id}`](${it.fileName})" }
+			val related = if (relatedIssuesText.isNotEmpty()) " along with ${relatedIssuesText}" else ""
 			appendLine(
 				"""
 					# `${issue.id}`
 					${issue.title}
 					
-					_Defined by `${rule.id}` in ${ruleSet.name} ruleset._
+					_Defined by `${rule.id}` in the "[${ruleSet.name}](../)" ruleset${related}._
 					
 				""".trimIndent()
 			)
@@ -45,3 +71,6 @@ internal class Generator(
 
 private val Rule.id: String
 	get() = this::class.simpleName ?: error("Invalid rule")
+
+private val Issue.fileName: String
+	get() = this.id + ".md"
