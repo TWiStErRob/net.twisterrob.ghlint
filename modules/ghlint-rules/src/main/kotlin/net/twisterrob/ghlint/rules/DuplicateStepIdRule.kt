239e98e1af0810f6186c78b1c5b886c75a09258a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-25 18:16:16 +0000

    Add DuplicateStepIdRule

---
+++ modules/ghlint-rules/src/main/kotlin/net/twisterrob/ghlint/rules/DuplicateStepIdRule.kt
@@ -0,0 +1,146 @@
+package net.twisterrob.ghlint.rules
+
+import net.twisterrob.ghlint.model.Job
+import net.twisterrob.ghlint.rule.Example
+import net.twisterrob.ghlint.rule.Issue
+import net.twisterrob.ghlint.rule.Reporting
+import net.twisterrob.ghlint.rule.VisitorRule
+import net.twisterrob.ghlint.rule.report
+import net.twisterrob.ghlint.rules.utils.editDistance
+
+public class DuplicateStepIdRule : VisitorRule {
+
+	override val issues: List<Issue> = listOf(DuplicateStepId, SimilarStepId)
+
+	override fun visitNormalJob(reporting: Reporting, job: Job.NormalJob) {
+		super.visitNormalJob(reporting, job)
+		val ids = job.steps.mapNotNull { it.id }
+		val similar: Sequence<Triple<String, String, Int>> = ids
+			.combinations()
+			.map { (id1, id2) -> if (id1 < id2) Pair(id1, id2) else Pair(id2, id1) }
+			.map { Triple(it.first, it.second, editDistance(it.first, it.second)) }
+			.filter { it.third <= MAX_SHELLS_ON_STEPS }
+
+		similar.forEach { (id1, id2, distance) ->
+			if (distance == 0) {
+				reporting.report(DuplicateStepId, job) { "${it} has the `${id1}` step identifier multiple times." }
+			} else {
+				reporting.report(SimilarStepId, job) { "${it} has similar step identifiers: `${id1}` and `${id2}`." }
+			}
+		}
+	}
+
+	private companion object {
+
+		private const val MAX_SHELLS_ON_STEPS = 3
+
+		val DuplicateStepId = Issue(
+			id = "DuplicateStepId",
+			title = "Steps must have unique identifiers within a job.",
+			description = """
+				Multiple steps having the same identifier makes them unreferenceable.
+				The `id:` defined on a step becomes a key in the `steps` context.
+				If multiple steps have the same identifier, only one of them will be accessible.
+				Remove the one that is not needed, or rename it to be unique.
+				
+				Relevant documentation:
+				
+				 * [`jobs.<job_id>.steps[*].id`](https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idstepsid)
+				 * [`steps` context](https://docs.github.com/en/actions/learn-github-actions/contexts#steps-context)
+			""".trimIndent(),
+			compliant = listOf(
+				Example(
+					explanation = "Each step has a unique identifier or no identifier.",
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        id: my-step-id
+						      - run: echo "Example"
+						      - run: echo "Example"
+						        id: my-other-step-id
+						""".trimIndent(),
+				),
+			),
+			nonCompliant = listOf(
+				Example(
+					explanation = """
+						Multiple steps have the same identifier.
+						
+						This is very likely a copy-paste mistake.
+					""".trimIndent(),
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        id: my-step-id
+						      - run: echo "Example"
+						        id: my-step-id
+					""".trimIndent(),
+				),
+			),
+		)
+
+		val SimilarStepId = Issue(
+			id = "SimilarStepId",
+			title = "Steps should have distinguishable identifiers within a job.",
+			description = """
+				Multiple steps having very similar identifiers makes them hard to distinguish.
+				
+				It's hard to read and understand a workflow when steps have similar identifiers,
+				this is especially important for people with dyslexia or ADHD.
+				
+				The `id:` defined on a step becomes a key in the `steps` context,
+				so it's easy to mistakenly reference another unintended one.
+			""".trimIndent(),
+			compliant = listOf(
+				Example(
+					explanation = "Each step has a distinguishable identifier or no identifier.",
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        id: my-step-id
+						      - run: echo "Example"
+						      - run: echo "Example"
+						        id: my-other-step-id
+						""".trimIndent(),
+				),
+			),
+			nonCompliant = listOf(
+				Example(
+					explanation = "Identifier of the two steps are very similar.",
+					content = """
+						on: push
+						jobs:
+						  example:
+						    runs-on: ubuntu-latest
+						    steps:
+						      - run: echo "Example"
+						        id: my-step-id
+						      - run: echo "Example"
+						        id: wy-step-id
+					""".trimIndent(),
+				),
+			),
+		)
+	}
+}
+
+private fun List<String>.combinations(): Sequence<Pair<String, String>> =
+	sequence {
+		for (index1 in 0 until this@combinations.size) {
+			for (index2 in index1 + 1 until this@combinations.size) {
+				yield(this@combinations[index1] to this@combinations[index2])
+			}
+		}
+	}
---
+++ modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/DuplicateStepIdRuleTest.kt
@@ -0,0 +1,91 @@
+package net.twisterrob.ghlint.rules
+
+import io.kotest.matchers.shouldHave
+import net.twisterrob.ghlint.testing.check
+import net.twisterrob.ghlint.testing.noFindings
+import net.twisterrob.ghlint.testing.singleFinding
+import net.twisterrob.ghlint.testing.test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestFactory
+
+class DuplicateStepIdRuleTest {
+
+	@TestFactory fun metadata() = test(DuplicateStepIdRule::class)
+
+	@Test fun `passes when no ids are defined`() {
+		val results = check<DuplicateStepIdRule>(
+			"""
+				jobs:
+				  test:
+				    steps:
+				      - run: echo "Example"
+				      - run: echo "Example"
+				      - run: echo "Example"
+			""".trimIndent()
+		)
+
+		results shouldHave noFindings()
+	}
+
+	@Test fun `passes when same or similar ids are in different jobs`() {
+		val results = check<DuplicateStepIdRule>(
+			"""
+				jobs:
+				  test1:
+				    steps:
+				      - run: echo "Example"
+				        id: step-id
+				      - run: echo "Example"
+				        id: mystep1
+				  test2:
+				    steps:
+				      - run: echo "Example"
+				        id: step-id
+				      - run: echo "Example"
+				        id: mystep2
+			""".trimIndent()
+		)
+
+		results shouldHave noFindings()
+	}
+
+	@Test fun `reports when ids are similar`() {
+		val results = check<DuplicateStepIdRule>(
+			"""
+				jobs:
+				  test:
+				    steps:
+				      - run: echo "Example"
+				        id: test1
+				      - run: echo "Example"
+				        id: step
+				      - run: echo "Example"
+				        id: test2
+			""".trimIndent()
+		)
+
+		results shouldHave singleFinding(
+			"SimilarStepId",
+			"Job[test] has similar step identifiers: `test1` and `test2`.",
+		)
+	}
+
+	@Test fun `reports when ids are the same`() {
+		val results = check<DuplicateStepIdRule>(
+			"""
+				jobs:
+				  test:
+				    steps:
+				      - run: echo "Example"
+				        id: test
+				      - run: echo "Example"
+				        id: test
+			""".trimIndent()
+		)
+
+		results shouldHave singleFinding(
+			"DuplicateStepId",
+			"Job[test] has the `test` step identifier multiple times.",
+		)
+	}
+}
