2de18478db62008f2b14bc9bb9a6e0758d943d43
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-25 23:20:52 +0000

    Group reporting options

--- modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/CLI.kt
+++ modules/ghlint/src/main/kotlin/net/twisterrob/ghlint/CLI.kt
@@ -6,16 +6,24 @@ import com.github.ajalt.clikt.core.ProgramResult
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.help
 import com.github.ajalt.clikt.parameters.arguments.multiple
-import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.groups.OptionGroup
+import com.github.ajalt.clikt.parameters.groups.provideDelegate
 import com.github.ajalt.clikt.parameters.options.eagerOption
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.boolean
 import com.github.ajalt.clikt.parameters.types.path
 import java.nio.file.Path
 
-internal class CLI : CliktCommand(), Configuration {
+internal class CLI : CliktCommand(
+	name = "ghlint",
+	printHelpOnEmptyArgs = true,
+	help = """
+		GitHub Actions Linter (GH-Lint).
+		A tool to lint GitHub Actions workflows.
+		See https://ghlint.twisterrob.net for more.
+	""".trimIndent(),
+), Configuration {
 
 	override fun run() {
 		val code = Main().run(this)
@@ -35,25 +43,34 @@ internal class CLI : CliktCommand(), Configuration {
 		.multiple(required = true)
 		.help("File to scan.")
 
-	override val reportExitCode: Boolean by option("--exit")
-		.flag("--ignore-failures", default = true, defaultForHelp = "on")
-		.help("Exit with non-zero code if there are findings. Default: --exit.")
-
-	override val reportConsole: Boolean by option("--console")
-		.flag("--silent", default = true, defaultForHelp = "on")
-		.help("Output to console. --silent does not affect --verbose, only findings. Default: --console.")
-
-	override val reportSarif: Path? by option("--sarif")
-		// Theoretically mustBeWritable = true, but Java requires the file to exists, which is silly.
-		.path(canBeDir = false)
-		.help("Output a SARIF file.")
-
-	override val reportGitHubCommands: Boolean by option("--ghcommands")
-		.boolean()
-		.default(false)
-		.help("Output GitHub Commands (warnings).")
+	private val inputs: InputOptions by InputOptions()
 
 	override val verbose: Boolean by option("-v", "--verbose")
 		.flag(default = false, defaultForHelp = "off")
 		.help("Prints more information.")
+
+	override val reportExitCode: Boolean get() = inputs.reportExitCode
+	override val reportConsole: Boolean get() = inputs.reportConsole
+	override val reportSarif: Path? get() = inputs.reportSarif
+	override val reportGitHubCommands: Boolean get() = inputs.reportGitHubCommands
+
+	private class InputOptions : OptionGroup("Reporting") {
+
+		val reportExitCode: Boolean by option("--exit")
+			.flag("--ignore-failures", default = false)
+			.help("Exit with non-zero code if there are findings. Default: --ignore-failures.")
+
+		val reportConsole: Boolean by option("--console")
+			.flag("--silent", default = true)
+			.help("Output to console. Default: --console. --silent does not affect --verbose, only findings.")
+
+		val reportSarif: Path? by option("--sarif")
+			// Theoretically mustBeWritable = true, but Java requires the file to exists, which is silly.
+			.path(canBeDir = false)
+			.help("Output a SARIF file.")
+
+		val reportGitHubCommands: Boolean by option("--ghcommands")
+			.flag()
+			.help("Output GitHub Commands (warnings).")
+	}
 }
