7479d4bc89178f28ab1bdedeb34a720c50baa0a7
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-02-27 22:42:52 +0000

    Retrieve schema only once

--- modules/ghlint-jsonschema/src/main/kotlin/net/twisterrob/ghlint/yaml/YamlValidation.kt
+++ modules/ghlint-jsonschema/src/main/kotlin/net/twisterrob/ghlint/yaml/YamlValidation.kt
@@ -1,23 +1,35 @@
 package net.twisterrob.ghlint.yaml
 
 import dev.harrel.jsonschema.Error
+import dev.harrel.jsonschema.SchemaResolver
 import dev.harrel.jsonschema.Validator
 import dev.harrel.jsonschema.ValidatorFactory
 import org.intellij.lang.annotations.Language
+import java.net.URI
 import java.net.URL
 
 public object YamlValidation {
 
+	private const val WORKFLOW_SCHEMA_URL =
+		"https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json"
+
+	private val resolver = object : SchemaResolver {
+		private val cache: MutableMap<String, SchemaResolver.Result> = mutableMapOf()
+
+		override fun resolve(uri: String): SchemaResolver.Result =
+			cache.getOrPut(uri) {
+				SchemaResolver.Result.fromString(URL(uri).readText())
+			}
+	}
+
 	public fun validate(@Language("yaml") yaml: String): Validator.Result {
-		val url =
-			"https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/github-workflow.json"
 		val factory = SnakeYamlJsonNode.Factory(Yaml::load)
 		val validator = ValidatorFactory()
 			.withDisabledSchemaValidation(true)
 			.withJsonNodeFactory(factory)
+			.withSchemaResolver(resolver)
 			.createValidator()
-		val schema = validator.registerSchema(URL(url).readText())
-		return validator.validate(schema, factory.create(yaml))
+		return validator.validate(URI.create(WORKFLOW_SCHEMA_URL), factory.create(yaml))
 	}
 }
 
