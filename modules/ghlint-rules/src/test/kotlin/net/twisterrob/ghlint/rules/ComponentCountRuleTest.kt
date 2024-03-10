9b840b6a8be5822a67b05fb4ce84e502b7c2778b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-03-10 21:29:50 +0000

    Move utilities down

--- modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/ComponentCountRuleTest.kt
+++ modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/ComponentCountRuleTest.kt
@@ -16,85 +16,6 @@ class ComponentCountRuleTest {
 
 	@TestFactory fun metadata() = test(ComponentCountRule::class)
 
-	private fun Random.generateJobs(count: Int): String =
-		"\n" + (1..count).joinToString(separator = "\n") {
-			when (random().toInt() % 2) {
-				0 -> """
-					test${it}:
-					  runs-on: ubuntu-latest
-					  steps:${generateSteps(nextInt(1, 5))}
-				""".trimIndent()
-
-				1 -> """
-					test${it}:
-					  uses: org/repo/.github/workflows/workflow.yml@v${it}
-				""".trimIndent()
-
-				else -> error("Not possible")
-			}
-		}.prependIndent("\t\t\t\t\t  ")
-
-	private fun Random.generateSteps(count: Int): String =
-		"\n" + (1..count).joinToString(separator = "\n") {
-			when (nextInt(1, 3)) {
-				1 -> "- run: echo \"Test $it\""
-				2 -> "- uses: some/action@v${it}"
-				else -> error("Not possible")
-			}
-		}.prependIndent("\t\t\t\t\t    ")
-
-	@Nested
-	@Suppress("TrimMultilineRawString") // Verifying exact indentations.
-	inner class GenerateTest {
-
-		@Test fun `generate single step`() {
-			Random(0).generateSteps(1) shouldBe """
-					    - uses: some/action@v1
-			""".trimEnd()
-		}
-
-		@Test fun `generate multiple steps`() {
-			Random(0).generateSteps(3) shouldBe """
-					    - uses: some/action@v1
-					    - run: echo "Test 2"
-					    - uses: some/action@v3
-			""".trimEnd()
-		}
-
-		@Test fun `generate single job`() {
-			Random(0).generateJobs(1) shouldBe """
-					  test1:
-					    runs-on: ubuntu-latest
-					    steps:
-					      - run: echo "Test 1"
-					      - uses: some/action@v2
-					      - uses: some/action@v3
-			""".trimEnd()
-		}
-
-		@Test fun `generate multiple jobs`() {
-			Random(0).generateJobs(3) shouldBe """
-					  test1:
-					    runs-on: ubuntu-latest
-					    steps:
-					      - run: echo "Test 1"
-					      - uses: some/action@v2
-					      - uses: some/action@v3
-					  test2:
-					    runs-on: ubuntu-latest
-					    steps:
-					      - run: echo "Test 1"
-					      - run: echo "Test 2"
-					      - run: echo "Test 3"
-					  test3:
-					    runs-on: ubuntu-latest
-					    steps:
-					      - uses: some/action@v1
-					      - run: echo "Test 2"
-			""".trimEnd()
-		}
-	}
-
 	@Nested
 	inner class TooManyJobsTest {
 
@@ -234,4 +155,83 @@ class ComponentCountRuleTest {
 			)
 		}
 	}
+
+	@Nested
+	@Suppress("TrimMultilineRawString") // Verifying exact indentations.
+	inner class GenerateTest {
+
+		@Test fun `generate single step`() {
+			Random(0).generateSteps(1) shouldBe """
+					    - uses: some/action@v1
+			""".trimEnd()
+		}
+
+		@Test fun `generate multiple steps`() {
+			Random(0).generateSteps(3) shouldBe """
+					    - uses: some/action@v1
+					    - run: echo "Test 2"
+					    - uses: some/action@v3
+			""".trimEnd()
+		}
+
+		@Test fun `generate single job`() {
+			Random(0).generateJobs(1) shouldBe """
+					  test1:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - run: echo "Test 1"
+					      - uses: some/action@v2
+					      - uses: some/action@v3
+			""".trimEnd()
+		}
+
+		@Test fun `generate multiple jobs`() {
+			Random(0).generateJobs(3) shouldBe """
+					  test1:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - run: echo "Test 1"
+					      - uses: some/action@v2
+					      - uses: some/action@v3
+					  test2:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - run: echo "Test 1"
+					      - run: echo "Test 2"
+					      - run: echo "Test 3"
+					  test3:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - uses: some/action@v1
+					      - run: echo "Test 2"
+			""".trimEnd()
+		}
+	}
+
+	private fun Random.generateJobs(count: Int): String =
+		"\n" + (1..count).joinToString(separator = "\n") {
+			when (random().toInt() % 2) {
+				0 -> """
+					test${it}:
+					  runs-on: ubuntu-latest
+					  steps:${generateSteps(nextInt(1, 5))}
+				""".trimIndent()
+
+				1 -> """
+					test${it}:
+					  uses: org/repo/.github/workflows/workflow.yml@v${it}
+				""".trimIndent()
+
+				else -> error("Not possible")
+			}
+		}.prependIndent("\t\t\t\t\t  ")
+
+	private fun Random.generateSteps(count: Int): String =
+		"\n" + (1..count).joinToString(separator = "\n") {
+			when (nextInt(1, 3)) {
+				1 -> "- run: echo \"Test $it\""
+				2 -> "- uses: some/action@v${it}"
+				else -> error("Not possible")
+			}
+		}.prependIndent("\t\t\t\t\t    ")
 }
