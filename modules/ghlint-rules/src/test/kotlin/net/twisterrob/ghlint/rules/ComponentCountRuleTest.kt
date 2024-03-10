a2fc05b2166e63e45391bb274378e040c5803372
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-03-10 20:39:55 +0000

    Test generateJobs and generateSteps

--- modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/ComponentCountRuleTest.kt
+++ modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/ComponentCountRuleTest.kt
@@ -1,5 +1,6 @@
 package net.twisterrob.ghlint.rules
 
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldHave
 import net.twisterrob.ghlint.testing.check
 import net.twisterrob.ghlint.testing.noFindings
@@ -15,13 +16,13 @@ class ComponentCountRuleTest {
 
 	@TestFactory fun metadata() = test(ComponentCountRule::class)
 
-	private fun generateJobs(count: Int): String =
+	private fun Random.generateJobs(count: Int): String =
 		"\n" + (1..count).joinToString(separator = "\n") {
 			when (random().toInt() % 2) {
 				0 -> """
 					test${it}:
 					  runs-on: ubuntu-latest
-					  steps:${generateSteps(Random.nextInt(1, 5))}
+					  steps:${generateSteps(nextInt(1, 5))}
 				""".trimIndent()
 
 				1 -> """
@@ -33,14 +34,65 @@ class ComponentCountRuleTest {
 			}
 		}.prependIndent("\t\t\t\t\t  ")
 
-	private fun generateSteps(count: Int): String =
+	private fun Random.generateSteps(count: Int): String =
 		"\n" + (1..count).joinToString(separator = "\n") {
-			when (Random.nextInt(1, 3)) {
+			when (nextInt(1, 3)) {
 				1 -> "- run: echo \"Test $it\""
 				2 -> "- uses: some/action@v${it}"
 				else -> error("Not possible")
 			}
 		}.prependIndent("\t\t\t\t\t    ")
+
+	@Nested
+	inner class GenerateTest {
+
+		@Test fun `generate single step`() {
+			Random(0).generateSteps(1) shouldBe """
+					    - uses: some/action@v1
+			""".trimEnd()
+		}
+
+		@Test fun `generate multiple steps`() {
+			Random(0).generateSteps(3) shouldBe """
+					    - uses: some/action@v1
+					    - run: echo "Test 2"
+					    - uses: some/action@v3
+			""".trimEnd()
+		}
+
+		@Test fun `generate single job`() {
+			Random(0).generateJobs(1) shouldBe """
+					  test1:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - run: echo "Test 1"
+					      - uses: some/action@v2
+					      - uses: some/action@v3
+			""".trimEnd()
+		}
+
+		@Test fun `generate multiple jobs`() {
+			Random(0).generateJobs(3) shouldBe """
+					  test1:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - run: echo "Test 1"
+					      - uses: some/action@v2
+					      - uses: some/action@v3
+					  test2:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - run: echo "Test 1"
+					      - run: echo "Test 2"
+					      - run: echo "Test 3"
+					  test3:
+					    runs-on: ubuntu-latest
+					    steps:
+					      - uses: some/action@v1
+					      - run: echo "Test 2"
+			""".trimEnd()
+		}
+	}
 
 	@Nested
 	inner class TooManyJobsTest {
@@ -49,7 +101,7 @@ class ComponentCountRuleTest {
 		fun `passes single step job`() {
 			val results = check<ComponentCountRule>(
 				"""
-					jobs:${generateJobs(1)}
+					jobs:${Random.generateJobs(1)}
 				""".trimIndent()
 			)
 
@@ -60,7 +112,7 @@ class ComponentCountRuleTest {
 		fun `passes few step job`() {
 			val results = check<ComponentCountRule>(
 				"""
-					jobs:${generateJobs(5)}
+					jobs:${Random.generateJobs(5)}
 				""".trimIndent()
 			)
 
@@ -71,7 +123,7 @@ class ComponentCountRuleTest {
 		fun `passes max steps count`() {
 			val results = check<ComponentCountRule>(
 				"""
-					jobs:${generateJobs(10)}
+					jobs:${Random.generateJobs(10)}
 				""".trimIndent()
 			)
 
@@ -82,7 +134,7 @@ class ComponentCountRuleTest {
 		fun `fails max steps count + 1`() {
 			val results = check<ComponentCountRule>(
 				"""
-					jobs:${generateJobs(11)}
+					jobs:${Random.generateJobs(11)}
 				""".trimIndent()
 			)
 
@@ -96,7 +148,7 @@ class ComponentCountRuleTest {
 		fun `fails double steps count`() {
 			val results = check<ComponentCountRule>(
 				"""
-					jobs:${generateJobs(20)}
+					jobs:${Random.generateJobs(20)}
 				""".trimIndent()
 			)
 
@@ -116,7 +168,7 @@ class ComponentCountRuleTest {
 				"""
 					jobs:
 					  test:
-					    steps:${generateSteps(1)}
+					    steps:${Random.generateSteps(1)}
 				""".trimIndent()
 			)
 
@@ -129,7 +181,7 @@ class ComponentCountRuleTest {
 				"""
 					jobs:
 					  test:
-					    steps:${generateSteps(5)}
+					    steps:${Random.generateSteps(5)}
 				""".trimIndent()
 			)
 
@@ -142,7 +194,7 @@ class ComponentCountRuleTest {
 				"""
 					jobs:
 					  test:
-					    steps:${generateSteps(20)}
+					    steps:${Random.generateSteps(20)}
 				""".trimIndent()
 			)
 
@@ -155,7 +207,7 @@ class ComponentCountRuleTest {
 				"""
 					jobs:
 					  test:
-					    steps:${generateSteps(21)}
+					    steps:${Random.generateSteps(21)}
 				""".trimIndent()
 			)
 
@@ -171,7 +223,7 @@ class ComponentCountRuleTest {
 				"""
 					jobs:
 					  test:
-					    steps:${generateSteps(40)}
+					    steps:${Random.generateSteps(40)}
 				""".trimIndent()
 			)
 
