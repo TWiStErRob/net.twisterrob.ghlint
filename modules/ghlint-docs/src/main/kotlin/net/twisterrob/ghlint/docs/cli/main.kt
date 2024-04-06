9a7f114bc8559b6a35308adc3a3e6c559412528c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-06 11:56:26 +0100

    Extract md from Kotlin

--- modules/ghlint-docs/src/main/kotlin/net/twisterrob/ghlint/docs/cli/main.kt
+++ modules/ghlint-docs/src/main/kotlin/net/twisterrob/ghlint/docs/cli/main.kt
@@ -12,70 +12,14 @@ public fun main(vararg args: String) {
 	target.writeText(generateCliDocs(cliHelp))
 }
 
-@Suppress("detekt.LongMethod") // It's just text.
-private fun generateCliDocs(cliHelp: String): String =
-	buildString {
-		appendLine("# Command Line Interface")
-		appendLine(
-			"""
-				GH-Lint comes as an executable JAR file that can be run from the command line.
-			""".trimIndent()
-		)
+private fun generateCliDocs(cliHelp: String): String {
+	val cliFile = Resources::class.java.getResourceAsStream("/cli.md") ?: error("Cannot find `cli.md`")
+	val cliMarkdown = cliFile.reader().use { it.readText() }
+	return cliMarkdown
+		.replace("{{ghlint --help}}", cliHelp)
+}
 
-		appendLine("## Installation")
-		appendLine("### GitHub Releases")
-		appendLine(
-			"""
-				Download the latest "CLI executable" from the [GitHub Releases][releases] page.
-				
-				[releases]: https://github.com/TWiStErRob/net.twisterrob.ghlint/releases
-			""".trimIndent()
-		)
-
-		appendLine("### GitHub pre-releases")
-		appendLine(
-			"""
-				Most commits to the repository are built with GitHub Actions with a ready to use artifact.
-				To get the latest `-SNAPSHOT` version,
-				download the "CLI Application" artifact from [one of the workflow runs on `main`][snapshots].
-				It's possible to download the same artifacts for individual PR commits as well.
-				
-				[snapshots]: https://github.com/TWiStErRob/net.twisterrob.ghlint/actions/workflows/ci.yml?query=branch%3Amain
-			""".trimIndent()
-		)
-
-		appendLine("## Execution")
-		appendLine(
-			"""
-				Regardless of where you downloaded the JAR file, you can run it from the command line.
-				
-				The JAR file can be run with Java on all operating systems:
-				```shell
-				java -jar ghlint.jar
-				```
-				
-				On Unix systems (Linux, Mac), you can make the JAR executable and run it directly:
-				```shell
-				mv ghlint.jar ghlint
-				chmod +x ghlint
-				./ghlint --version
-				```
-				
-				On Windows, you can simulate the same behavior with a batch file (`ghlint.bat` in the same folder as the JAR file):
-				```batch
-				@echo off
-				java -jar "%~dp0\ghlint.jar" %*
-				```
-				
-				If you put the executable JAR (or BAT) on the `PATH`, you can run it from anywhere:
-				```shell
-				ghlint my-workflow.yml
-				```
-			""".trimIndent()
-		)
-
-		appendLine("## Usage")
-		appendLine("```text")
-		appendLine(cliHelp)
-		appendLine("```")
-	}
+/**
+ * Placeholder for accessing Java resources.
+ */
+private object Resources
