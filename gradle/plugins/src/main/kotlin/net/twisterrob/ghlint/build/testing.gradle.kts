488833e2a0a558fcc4d7add5f2d832f1a61a76e2
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-14 21:31:15 +0100

    Enable parallel test execution

--- gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/testing.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/testing.gradle.kts
@@ -2,6 +2,7 @@ package net.twisterrob.ghlint.build
 
 import net.twisterrob.ghlint.build.dsl.isCI
 import net.twisterrob.ghlint.build.dsl.libs
+import java.util.Properties
 
 plugins {
 	id("org.gradle.java")
@@ -21,6 +22,12 @@ testing.suites.withType<JvmTestSuite>().configureEach {
 				languageVersion.set(JavaLanguageVersion.of(libs.versions.java.toolchainTest.get()))
 			})
 			ignoreFailures = isCI.get()
+			systemProperties(
+				rootProject.file("config/junit/junit-platform.properties")
+					.reader()
+					.use { Properties().apply { load(it) } }
+					.mapKeys { (k, _) -> k.toString() }
+			)
 		}
 	}
 }
