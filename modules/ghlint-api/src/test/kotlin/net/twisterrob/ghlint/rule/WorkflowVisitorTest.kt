eb689b0564fa941eedfaa35b4fdfade90ccfc9c9
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-04-15 09:23:28 +0100

    WIP

--- modules/ghlint-api/src/test/kotlin/net/twisterrob/ghlint/rule/WorkflowVisitorTest.kt
+++ modules/ghlint-api/src/test/kotlin/net/twisterrob/ghlint/rule/WorkflowVisitorTest.kt
@@ -1,4 +1,23 @@
 package net.twisterrob.ghlint.rule
 
+import net.twisterrob.ghlint.model.Step
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.spy
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
+
 class WorkflowVisitorTest {
+
+	private val subject: WorkflowVisitor = spy(object : WorkflowVisitor {})
+	private val reporting: Reporting = mock()
+
+	@Test fun `visitRunStep does nothing`() {
+		val step: Step.Run = mock()
+
+		subject.visitRunStep(reporting, step)
+
+		verify(subject).visitRunStep(reporting, step)
+		verifyNoMoreInteractions(subject, reporting, step)
+	}
 }
