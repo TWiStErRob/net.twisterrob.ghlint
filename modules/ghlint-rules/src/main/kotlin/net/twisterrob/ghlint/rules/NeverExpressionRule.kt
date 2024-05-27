48a7f387b064d6dd38586312f443131986e45c60
Tim Searle <tim@searle.dev>
2024-05-27 09:22:11 +0100

    Resolving manual detekt failures

--- modules/ghlint-rules/src/main/kotlin/net/twisterrob/ghlint/rules/NeverExpressionRule.kt
+++ modules/ghlint-rules/src/main/kotlin/net/twisterrob/ghlint/rules/NeverExpressionRule.kt
@@ -10,39 +10,49 @@ import net.twisterrob.ghlint.rule.visitor.WorkflowVisitor
 
 public class NeverExpressionRule : VisitorRule, WorkflowVisitor {
 
-    override val issues : List<Issue> = listOf(NeverExpression)
+	override val issues: List<Issue> = listOf(NeverExpression)
 
-    private companion object {
-        const val uses = "actions/checkout@\${{ github.sha }}"
+	override fun visitWorkflowUsesStep(reporting: Reporting, step: WorkflowStep.Uses) {
+		super.visitWorkflowUsesStep(reporting, step)
 
-        val NeverExpression = Issue(
-            id = "NeverExpression",
-            title = "Expressions should not be used in uses field.",
-            description = """
-            > GitHub Action Expressions can be used to programmatically set environment variables in workflow files.
-            > -- [About expressions](https://docs.github.com/en/actions/learn-github-actions/expressions)
-            
-            However, they cannot be used within the uses field. GitHub will report an error - "The workflow is not valid: A template expression is not allowed in this context"
-        """.trimIndent(),
-            compliant = listOf(
-                Example(
-                    explanation = "GitHub Expression not used within the uses field.",
-                    path = "ci-build.yml",
-                    content = """
+		if (step.uses.uses.containsGitHubExpression()) {
+			reporting.report(NeverExpression, step) {
+				"$it uses a GitHub expression in the uses field."
+			}
+		}
+	}
+
+	private companion object {
+		const val uses = "actions/checkout@\${{ github.sha }}"
+
+		val NeverExpression = Issue(
+			id = "NeverExpression",
+			title = "Expressions should not be used in uses field.",
+			description = """
+				> GitHub Action Expressions can be used to programmatically set environment variables in workflow files.
+				> -- [About expressions](https://docs.github.com/en/actions/learn-github-actions/expressions)
+				
+				However, they cannot be used within the uses field. GitHub will report an error - "The workflow is not valid: A template expression is not allowed in this context"
+			""".trimIndent(),
+			compliant = listOf(
+				Example(
+					explanation = "GitHub Expression not used within the uses field.",
+					path = "ci-build.yml",
+					content = """
 						on: push
 						jobs:
 						  example:
 						    runs-on: ubuntu-latest
 						    steps:
-						      - uses: actions/checkout@v4"
+						      - uses: actions/checkout@v4
 					""".trimIndent(),
-                ),
-            ),
-            nonCompliant = listOf(
-                Example(
-                    explanation = "GitHub Expression used within the uses field.",
-                    path = "ci-build.yml",
-                    content = """
+					),
+			),
+			nonCompliant = listOf(
+				Example(
+					explanation = "GitHub Expression used within the uses field.",
+					path = "ci-build.yml",
+					content = """
 						on: push
 						jobs:
 						  example:
@@ -50,23 +60,14 @@ public class NeverExpressionRule : VisitorRule, WorkflowVisitor {
 						    steps:
 						      - uses: $uses
 					""".trimIndent(),
-                ),
-            )
-        )
-    }
-
-    override fun visitWorkflowUsesStep(reporting: Reporting, step: WorkflowStep.Uses) {
-        super.visitWorkflowUsesStep(reporting, step)
-
-        if (step.uses.uses.containsGitHubExpression()) {
-            reporting.report(NeverExpression, step) {
-                "$it uses a GitHub expression in the uses field."
-            }
-        }
-    }
+				),
+			)
+		)
+	}
 }
 
 internal fun String.containsGitHubExpression(): Boolean {
-    val regex = Regex(".*\\$\\{\\{.*}}.*")
-    return regex.containsMatchIn(this)
+	@Suppress("StringShouldBeRawString")
+	val regex = Regex(".*\\$\\{\\{.*}}.*")
+	return regex.containsMatchIn(this)
 }
--- modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/NeverExpressionRuleKtTest.kt
+++ modules/ghlint-rules/src/test/kotlin/net/twisterrob/ghlint/rules/NeverExpressionRuleKtTest.kt
@@ -4,55 +4,57 @@ import io.kotest.matchers.shouldHave
 import net.twisterrob.ghlint.testing.noFindings
 import net.twisterrob.ghlint.testing.singleFinding
 import net.twisterrob.ghlint.testing.test
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 
 class NeverExpressionRuleKtTest {
 
-    @TestFactory fun metadata() = test(NeverExpressionRule::class)
-    @Test fun `passes when no expression in uses field`() {
-        val results = net.twisterrob.ghlint.testing.check<NeverExpressionRule>(
+	@TestFactory fun metadata() = test(NeverExpressionRule::class)
+	@Test fun `passes when no expression in uses field`() {
+		val results = net.twisterrob.ghlint.testing.check<NeverExpressionRule>(
             """
-				on: push
-				jobs:
-				  test:
-				    runs-on: test
-				    steps:
-				      - uses: actions/checkout@v4
-			""".trimIndent(),
-        )
+             on: push
+             jobs:
+               test:
+                 runs-on: test
+                 steps:
+                 - uses: actions/checkout@v4
+            """.trimIndent(),
+		)
 
-        results shouldHave noFindings()
-    }
+		results shouldHave noFindings()
+	}
 
-    @Test fun `reports when expression in uses field`() {
-        val uses = "actions/checkout@\${{ github.sha }}"
-        val results = net.twisterrob.ghlint.testing.check<NeverExpressionRule>(
+	@Test fun `reports when expression in uses field`() {
+		val uses = "actions/checkout@\${{ github.sha }}"
+		val results = net.twisterrob.ghlint.testing.check<NeverExpressionRule>(
             """
-				on: push
-				jobs:
-				  test:
-				    runs-on: test
-				    steps:
-				      - uses: actions/checkout@\${uses}
-			""".trimIndent(),
-        )
+             on: push
+             jobs:
+               test:
+                 runs-on: test
+                 steps:
+                 - uses: actions/checkout@\${uses}
+            """.trimIndent(),
+		)
 
-        results shouldHave singleFinding(
+		results shouldHave singleFinding(
             "NeverExpression",
-            "Step[actions/checkout@\\actions/checkout@\${{ github.sha }}] in Job[test] uses a GitHub expression in the uses field.")
-    }
+            "Step[actions/checkout@\$uses] in Job[test] uses a GitHub expression in the uses field."
+        )
+	}
 
-    @Test fun `containsExpression should return true for expression`() {
-        val uses = "owner/repo@\${{ github.sha }}"
-        val result = uses.containsGitHubExpression()
-        assertTrue(result)
-    }
+	@Test fun `containsExpression should return true for expression`() {
+		val uses = "owner/repo@\${{ github.sha }}"
+		val isGitHubExpression = uses.containsGitHubExpression()
+		assertTrue(isGitHubExpression)
+	}
 
-    @Test fun `containsExpression should return false for no expression`() {
-        val uses = "owner/repo@main"
-        val result = uses.containsGitHubExpression()
-        assertFalse(result)
-    }
+	@Test fun `containsExpression should return false for no expression`() {
+		val uses = "owner/repo@main"
+		val isGitHubExpression = uses.containsGitHubExpression()
+		assertFalse(isGitHubExpression)
+	}
 }
