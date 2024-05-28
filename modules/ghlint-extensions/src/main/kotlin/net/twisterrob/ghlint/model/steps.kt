4f623e2e4a11b0632070028f5b6ccfb36c9fcc9b
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-05-28 21:11:52 +0100

    Be explicit

--- modules/ghlint-extensions/src/main/kotlin/net/twisterrob/ghlint/model/steps.kt
+++ modules/ghlint-extensions/src/main/kotlin/net/twisterrob/ghlint/model/steps.kt
@@ -1,7 +1,7 @@
 package net.twisterrob.ghlint.model
 
 public val Step.stepsBefore: List<Step>
-	get() = this.parent.steps.subList(0, index.value)
+	get() = this.parent.steps.subList(0, this.index.value)
 
 public val Step.stepsAfter: List<Step>
-	get() = this.parent.steps.let { it.subList(index.value + 1, it.size) }
+	get() = this.parent.steps.let { it.subList(this.index.value + 1, it.size) }
