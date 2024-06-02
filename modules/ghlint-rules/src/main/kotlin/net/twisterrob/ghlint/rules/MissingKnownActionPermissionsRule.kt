0ce9e377963b7b3f1110d0c2ce9b418dab176864
Tim Searle <tim@searle.dev>
2024-06-02 18:02:08 +0100

    Add compliant/non-compliant example

--- modules/ghlint-rules/src/main/kotlin/net/twisterrob/ghlint/rules/MissingKnownActionPermissionsRule.kt
+++ modules/ghlint-rules/src/main/kotlin/net/twisterrob/ghlint/rules/MissingKnownActionPermissionsRule.kt
@@ -5,6 +5,7 @@ import net.twisterrob.ghlint.model.Permission
 import net.twisterrob.ghlint.model.Scope
 import net.twisterrob.ghlint.model.WorkflowStep
 import net.twisterrob.ghlint.model.asEffectivePermissionsSet
+import net.twisterrob.ghlint.rule.Example
 import net.twisterrob.ghlint.rule.Issue
 import net.twisterrob.ghlint.rule.Reporting
 import net.twisterrob.ghlint.rule.report
@@ -40,10 +41,46 @@ public class MissingKnownActionPermissionsRule : VisitorRule, WorkflowVisitor {
 				id = "MissingRequiredActionPermissions",
 				title = "Required permissions are not declared for action.",
 				description = """
-					to be written
+					Certain GitHub Actions require specific permissions to be specified for the GITHUB_TOKEN to work correctly.
+					
+					For example, the `actions/checkout` action requires `contents: read` permission to be able to
+					check out the repository.
+					
+					References:
+					
+				 	* [Documentation of `GITHUB_TOKEN` permissions](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#modifying-the-permissions-for-the-github_token)
+				 	* [List of Available permissions](https://docs.github.com/en/actions/security-guides/automatic-token-authentication#permissions-for-the-github_token)
 				""".trimIndent(),
-				compliant = emptyList(),
-				nonCompliant = emptyList(),
+				compliant = listOf(
+					Example(
+						explanation = "Required permissions are explicitly declared on the job.",
+						content = """
+							on: push
+							jobs:
+							  example:
+							    runs-on: ubuntu-latest
+							    permissions:
+							      contents: read
+							    steps:
+							      - uses: actions/checkout@v4
+						""".trimIndent(),
+					),
+				),
+				nonCompliant = listOf(
+					Example(
+						explanation = "Required permissions are not declared on the job or workflow.",
+						content = """
+							on: push
+							jobs:
+							  example:
+							    runs-on: ubuntu-latest
+							    permissions:
+							      pull-requests: write
+							    steps:
+							      - uses: actions/checkout@v4
+						""".trimIndent(),
+				),
+			),
 		)
 	}
 }
