06433a1bca810bf468cf67a7bb4c6282c99685c8
Róbert Papp <papp.robert.s@gmail.com>
2024-06-02 18:45:02 +0100

    Push validation into load so it happens at more locations in test (#296)

--- modules/ghlint-testing/src/main/kotlin/net/twisterrob/ghlint/testing/validate.kt
+++ modules/ghlint-testing/src/main/kotlin/net/twisterrob/ghlint/testing/validate.kt
@@ -1,13 +1,15 @@
 package net.twisterrob.ghlint.testing
 
 import net.twisterrob.ghlint.BuiltInRuleSet
+import net.twisterrob.ghlint.model.File
 import net.twisterrob.ghlint.results.Finding
 import org.intellij.lang.annotations.Language
 
 internal fun validate(
 	@Language("yaml") yaml: String,
 	fileName: String = "test.yml",
-): List<Finding> {
-	val file = load(yaml, fileName)
-	return BuiltInRuleSet().createRules().flatMap { it.check(file) }
-}
+): List<Finding> =
+	validate(loadUnsafe(yaml, fileName))
+
+internal fun validate(file: File): List<Finding> =
+	BuiltInRuleSet().createRules().flatMap { it.check(file) }
