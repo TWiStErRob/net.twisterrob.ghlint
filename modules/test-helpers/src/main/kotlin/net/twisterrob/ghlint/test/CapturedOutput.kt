50fe84c6d9906317945f8c291a2175e019191bd5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-06-02 23:14:25 +0100

    Capture output helper 2

---
+++ modules/test-helpers/src/main/kotlin/net/twisterrob/ghlint/test/CapturedOutput.kt
@@ -0,0 +1,109 @@
+@file:OptIn(ExperimentalContracts::class)
+
+package net.twisterrob.ghlint.test
+
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
+public class CapturedOutput<T>(
+	public val result: T,
+	public val out: String,
+	public val err: String,
+)
+
+/**
+ * Captures the output of [System.out]/[System.err] during the execution of [block].
+ *
+ * In parallel test execution, lock the [System.setOut]/[System.setErr] usages, e.g. Jupiter:
+ * ```
+ * @ResourceLock(value = Resources.SYSTEM_OUT, mode = ResourceAccessMode.READ_WRITE)
+ * @ResourceLock(value = Resources.SYSTEM_ERR, mode = ResourceAccessMode.READ_WRITE)
+ * ```
+ *
+ * @sample net.twisterrob.ghlint.test.CapturedOutputTest.Output.exampleUsage
+ */
+public inline fun <R> captureSystemStreams(block: () -> R): CapturedOutput<R> {
+	contract {
+		callsInPlace(block, kotlin.contracts.InvocationKind.EXACTLY_ONCE)
+	}
+	val result = captureSystemOut { captureSystemErr { block() } }
+	return CapturedOutput(
+		result = result.result.result,
+		out = result.out,
+		err = result.result.err,
+	)
+}
+
+/**
+ * Captures the output of [System.out] during the execution of [block].
+ *
+ * In parallel test execution, lock the [System.setOut] usages, e.g. Jupiter:
+ * ```
+ * @ResourceLock(value = Resources.SYSTEM_OUT, mode = ResourceAccessMode.READ_WRITE)
+ * ```
+ *
+ * @sample net.twisterrob.ghlint.test.CapturedOutputTest.Output.exampleUsage
+ */
+public inline fun <R> captureSystemOut(block: () -> R): CapturedOutput<R> {
+	contract {
+		callsInPlace(block, kotlin.contracts.InvocationKind.EXACTLY_ONCE)
+	}
+	val out = System.out
+	val buffer = ByteArrayOutputStream()
+	System.setOut(PrintStream(buffer, true))
+	val result = try {
+		block()
+	} finally {
+		System.setOut(out)
+	}
+	return CapturedOutput(
+		result = result,
+		out = buffer.toString(Charsets.UTF_8),
+		err = "",
+	)
+}
+
+/**
+ * Captures the output of [System.out] during the execution of [block], bug does not capture the [block]'s result.
+ *
+ * @see captureSystemOut
+ */
+public inline fun captureSystemOutOnly(block: () -> Unit): String = captureSystemOut { block() }.out
+
+/**
+ * Captures the output of [System.err] during the execution of [block].
+ *
+ * In parallel test execution, lock the [System.setErr] usages, e.g. Jupiter:
+ * ```
+ * @ResourceLock(value = Resources.SYSTEM_ERR, mode = ResourceAccessMode.READ_WRITE)
+ * ```
+ *
+ * @sample net.twisterrob.ghlint.test.CapturedOutputTest.Error.exampleUsage
+ */
+public inline fun <R> captureSystemErr(block: () -> R): CapturedOutput<R> {
+	contract {
+		callsInPlace(block, kotlin.contracts.InvocationKind.EXACTLY_ONCE)
+	}
+	val err = System.err
+	val buffer = ByteArrayOutputStream()
+	System.setErr(PrintStream(buffer, true))
+	val result = try {
+		block()
+	} finally {
+		System.setErr(err)
+	}
+	return CapturedOutput(
+		result = result,
+		out = "",
+		err = buffer.toString(Charsets.UTF_8),
+	)
+}
+
+/**
+ * Captures the output of [System.err] during the execution of [block], bug does not capture the [block]'s result.
+ *
+ * @see captureSystemErr
+ */
+public inline fun captureSystemErrOnly(block: () -> Unit): String = captureSystemErr { block() }.err
