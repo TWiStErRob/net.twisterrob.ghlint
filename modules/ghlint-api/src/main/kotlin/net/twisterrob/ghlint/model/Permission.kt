2cc85249d33cb685845fbed1e62efd5906117f75
Tim Searle <tim@searle.dev>
2024-06-02 16:55:31 +0100

    Extract interface to own file

--- modules/ghlint-api/src/main/kotlin/net/twisterrob/ghlint/model/Permission.kt
+++ modules/ghlint-api/src/main/kotlin/net/twisterrob/ghlint/model/Permission.kt
@@ -1,27 +1,5 @@
 package net.twisterrob.ghlint.model
 
-import java.util.Locale
-
-// https://docs.github.com/en/actions/security-guides/automatic-token-authentication#permissions-for-the-github_token
-public interface Permissions {
-	public val actions: Access
-	public val attestations: Access
-	public val checks: Access
-	public val contents: Access
-	public val deployments: Access
-	public val idToken: Access
-	public val issues: Access
-	public val metadata: Access
-	public val packages: Access
-	public val pages: Access
-	public val pullRequests: Access
-	public val repositoryProjects: Access
-	public val securityEvents: Access
-	public val statuses: Access
-
-	public fun asMap(): Map<String, String>
-}
-
 public enum class Permission(public val value: String) {
 	ACTIONS("actions"),
 	ATTESTATIONS("attestations"),
