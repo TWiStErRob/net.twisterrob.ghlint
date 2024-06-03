34495c3c838f2700f69b2bff67d0beea3e16dbaa
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-06-03 23:12:08 +0100

    Add tests for each getter

--- modules/ghlint-snakeyaml/src/test/kotlin/net/twisterrob/ghlint/model/SnakePermissionsTest.kt
+++ modules/ghlint-snakeyaml/src/test/kotlin/net/twisterrob/ghlint/model/SnakePermissionsTest.kt
@@ -17,16 +17,40 @@ class SnakePermissionsTest {
 				jobs:
 				  test:
 				    permissions:
+				      actions: write
+				      attestations: read
+				      checks: write
 				      contents: read
+				      deployments: none
+				      id-token: read
 				      issues: write
+				      metadata: read
+				      packages: write
+				      pages: write
+				      pull-requests: write
+				      repository-projects: read
+				      security-events: write
+				      statuses: write
 				    runs-on: ubuntu-latest
 				    steps:
 				      - uses: actions/checkout@v4
 			""".trimIndent()
 		).asJob()
 
+		job.permissions?.actions shouldBe Access.WRITE
+		job.permissions?.attestations shouldBe Access.READ
+		job.permissions?.checks shouldBe Access.WRITE
 		job.permissions?.contents shouldBe Access.READ
+		job.permissions?.deployments shouldBe Access.NONE
+		job.permissions?.idToken shouldBe Access.READ
 		job.permissions?.issues shouldBe Access.WRITE
+		job.permissions?.metadata shouldBe Access.READ
+		job.permissions?.packages shouldBe Access.WRITE
+		job.permissions?.pages shouldBe Access.WRITE
+		job.permissions?.pullRequests shouldBe Access.WRITE
+		job.permissions?.repositoryProjects shouldBe Access.READ
+		job.permissions?.securityEvents shouldBe Access.WRITE
+		job.permissions?.statuses shouldBe Access.WRITE
 	}
 
 	@Test fun `workflow has permissions with correct values`() {
@@ -34,8 +58,20 @@ class SnakePermissionsTest {
 			"""
 				on: push
 				permissions:
-				  pull-requests: write
-				  id-token: read
+				  actions: read
+				  attestations: write
+				  checks: read
+				  contents: write
+				  deployments: write
+				  id-token: none
+				  issues: read
+				  metadata: write
+				  packages: read
+				  pages: read
+				  pull-requests: read
+				  repository-projects: write
+				  security-events: read
+				  statuses: read
 				jobs:
 				  test:
 				    runs-on: ubuntu-latest
@@ -44,8 +80,20 @@ class SnakePermissionsTest {
 			""".trimIndent()
 		).content as Workflow
 
-		workflow.permissions?.pullRequests shouldBe Access.WRITE
-		workflow.permissions?.idToken shouldBe Access.READ
+		workflow.permissions?.actions shouldBe Access.READ
+		workflow.permissions?.attestations shouldBe Access.WRITE
+		workflow.permissions?.checks shouldBe Access.READ
+		workflow.permissions?.contents shouldBe Access.WRITE
+		workflow.permissions?.deployments shouldBe Access.WRITE
+		workflow.permissions?.idToken shouldBe Access.NONE
+		workflow.permissions?.issues shouldBe Access.READ
+		workflow.permissions?.metadata shouldBe Access.WRITE
+		workflow.permissions?.packages shouldBe Access.READ
+		workflow.permissions?.pages shouldBe Access.READ
+		workflow.permissions?.pullRequests shouldBe Access.READ
+		workflow.permissions?.repositoryProjects shouldBe Access.WRITE
+		workflow.permissions?.securityEvents shouldBe Access.READ
+		workflow.permissions?.statuses shouldBe Access.READ
 	}
 
 	@Test fun `job with no permissions is null`() {
