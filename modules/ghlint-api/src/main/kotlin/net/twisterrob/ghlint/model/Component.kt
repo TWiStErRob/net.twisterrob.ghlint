917f52194c11e3b52bd1f900123d7435787e2605
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-06-05 00:32:27 +0100

    Docs and unused method

--- modules/ghlint-api/src/main/kotlin/net/twisterrob/ghlint/model/Component.kt
+++ modules/ghlint-api/src/main/kotlin/net/twisterrob/ghlint/model/Component.kt
@@ -1,18 +1,11 @@
 package net.twisterrob.ghlint.model
 
 /**
+ * Represents a component of GitHub Actions.
+ *
  * https://docs.github.com/en/actions/learn-github-actions/understanding-github-actions#the-components-of-github-actions
  */
 public sealed interface Component : Element {
 
 	public companion object
 }
-
-public val Component.file: File
-	get() = when (this) {
-		is Workflow -> parent
-		is Job -> parent.parent
-		is WorkflowStep -> parent.parent.parent
-		is Action -> parent
-		is ActionStep -> parent.parent.parent
-	}
