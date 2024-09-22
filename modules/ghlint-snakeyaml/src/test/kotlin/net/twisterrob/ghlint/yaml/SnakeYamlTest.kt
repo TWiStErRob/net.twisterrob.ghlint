c43d89299ec57847f8b3f9a0b41afb259fb9645c
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-09-22 14:53:03 +0100

    Add some tests to cover assumptions about snakeyaml

--- modules/ghlint-snakeyaml/src/test/kotlin/net/twisterrob/ghlint/yaml/SnakeYamlTest.kt
+++ modules/ghlint-snakeyaml/src/test/kotlin/net/twisterrob/ghlint/yaml/SnakeYamlTest.kt
@@ -2,8 +2,12 @@ package net.twisterrob.ghlint.yaml
 
 import io.kotest.matchers.shouldBe
 import net.twisterrob.ghlint.testing.yaml
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertInstanceOf
+import org.junit.jupiter.api.assertThrows
+import org.snakeyaml.engine.v2.nodes.ScalarNode
 
 class SnakeYamlTest {
 
@@ -117,6 +121,42 @@ class SnakeYamlTest {
 				
 			""".trimIndent()
 			reserialize(yaml)
+		}
+	}
+
+	@Nested
+	inner class EdgeCases {
+		@Test fun empty() {
+			val node = SnakeYaml.loadRaw(yaml("", "test.yml"))
+			assertInstanceOf<ScalarNode>(node)
+			assertEquals("", node.value)
+		}
+
+		@Test fun newline() {
+			val node = SnakeYaml.loadRaw(yaml("\n", "test.yml"))
+			assertInstanceOf<ScalarNode>(node)
+			assertEquals("", node.value)
+		}
+
+		@Test fun tab() {
+			val node = SnakeYaml.loadRaw(yaml("\t", "test.yml"))
+			assertInstanceOf<ScalarNode>(node)
+			assertEquals("", node.value)
+		}
+
+		@Test fun tabs() {
+			val ex = assertThrows<IllegalArgumentException> {
+				SnakeYaml.loadRaw(yaml("\t\t", "test.yml"))
+			}
+			val error = """
+				Failed to parse YAML: while scanning for the next token
+				found character '\t(TAB)' that cannot start any token. (Do not use \t(TAB) for indentation)
+				 in reader, line 1, column 1:
+				    		
+				    ^
+				
+			""".trimIndent()
+			assertEquals(error, ex.message)
 		}
 	}
 
