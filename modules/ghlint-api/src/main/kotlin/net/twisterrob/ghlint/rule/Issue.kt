536d14d727a810b9e1a2adcf866c8f46871bf493
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-06-27 02:13:26 +0100

    Fix compiler warning and report https://github.com/detekt/detekt/issues/8212

--- modules/ghlint-api/src/main/kotlin/net/twisterrob/ghlint/rule/Issue.kt
+++ modules/ghlint-api/src/main/kotlin/net/twisterrob/ghlint/rule/Issue.kt
@@ -7,20 +7,22 @@ import org.intellij.lang.annotations.Language
  *
  * For a template to create an issue, see [ExampleRule].
  */
+@Suppress("detekt.UnnecessaryAnnotationUseSiteTarget") // TODEL https://github.com/detekt/detekt/issues/8212
 public class Issue(
 	public val id: String,
-	@Language("markdown")
+	@param:Language("markdown")
 	public val title: String,
-	@Language("markdown")
+	@param:Language("markdown")
 	public val description: String,
 	public val compliant: List<Example>,
 	public val nonCompliant: List<Example>,
 )
 
+@Suppress("detekt.UnnecessaryAnnotationUseSiteTarget") // TODEL https://github.com/detekt/detekt/issues/8212
 public class Example(
 	public val path: String = "example.yml",
-	@Language("yaml")
+	@param:Language("yaml")
 	public val content: String,
-	@Language("markdown")
+	@param:Language("markdown")
 	public val explanation: String,
 )
