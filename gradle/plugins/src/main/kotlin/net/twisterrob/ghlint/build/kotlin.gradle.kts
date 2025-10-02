42118a75132b1bf09b4333f9e6780466ef0ce1c5
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-10-02 01:57:40 +0100

    more options

--- gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/kotlin.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/kotlin.gradle.kts
@@ -1,6 +1,7 @@
 package net.twisterrob.ghlint.build
 
 import net.twisterrob.ghlint.build.dsl.libs
+import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
 
 plugins {
 	id("org.jetbrains.kotlin.jvm")
@@ -10,10 +11,13 @@ kotlin {
 	explicitApi()
 	jvmToolchain(libs.versions.java.toolchain.map(String::toInt).get())
 	compilerOptions {
+		jvmDefault = JvmDefaultMode.ENABLE
 		allWarningsAsErrors = true
 		freeCompilerArgs.add("-opt-in=kotlin.ExperimentalStdlibApi")
 		freeCompilerArgs.add("-Xcontext-parameters")
-		freeCompilerArgs.add("-Xjvm-default=all")
+		// Kotlin 2.1: Make it easier to suppress warnings locally in the code by providing a name in logs.
+		// See https://youtrack.jetbrains.com/issue/KT-8087
+		freeCompilerArgs.add("-Xrender-internal-diagnostic-names")
 	}
 }
 
