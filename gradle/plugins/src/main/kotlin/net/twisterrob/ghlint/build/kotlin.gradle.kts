ed8be662491534608105677e5b27fc5d2834ab9d
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2025-10-02 01:30:18 +0100

    don't configure tasks in KGP

--- gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/kotlin.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/kotlin.gradle.kts
@@ -1,7 +1,6 @@
 package net.twisterrob.ghlint.build
 
 import net.twisterrob.ghlint.build.dsl.libs
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.jetbrains.kotlin.jvm")
@@ -9,22 +8,18 @@ plugins {
 
 kotlin {
 	jvmToolchain(libs.versions.java.toolchain.map(String::toInt).get())
+	compilerOptions {
+		allWarningsAsErrors = true
+		verbose = true
+		freeCompilerArgs.add("-opt-in=kotlin.ExperimentalStdlibApi")
+		freeCompilerArgs.add("-Xcontext-parameters")
+		freeCompilerArgs.add("-Xjvm-default=all")
+	}
 }
 
 dependencies {
 	implementation(libs.kotlin.stdlib)
 	plugins.withId("org.gradle.java-test-fixtures") {
 		add("testFixturesImplementation", libs.kotlin.stdlib)
-	}
-}
-
-// TODO kotlin.target.compilerOptions { ... }
-tasks.withType<KotlinCompile>().configureEach {
-	compilerOptions {
-		allWarningsAsErrors.set(true)
-		verbose.set(true)
-		freeCompilerArgs.add("-opt-in=kotlin.ExperimentalStdlibApi")
-		freeCompilerArgs.add("-Xcontext-parameters")
-		freeCompilerArgs.add("-Xjvm-default=all")
 	}
 }
