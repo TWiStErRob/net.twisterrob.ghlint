46910d598aa819c7fae893fe09c3f6e8554d19e0
Róbert Papp <papp.robert.s@gmail.com>
2026-02-21 17:47:10 +0000

    Hide "Sharing is only supported for boot loader classes because bootstrap classpath has been appended" warnings (#719)

--- gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/testing.gradle.kts
+++ gradle/plugins/src/main/kotlin/net/twisterrob/ghlint/build/testing.gradle.kts
@@ -29,6 +29,10 @@ testing.suites.withType<JvmTestSuite>().configureEach {
 					.use { Properties().apply { load(it) } }
 					.mapKeys { (k, _) -> k.toString() }
 			)
+			// Hide (https://stackoverflow.com/a/79098701/253468)
+			// > Java HotSpot(TM) 64-Bit Server VM warning:
+			// > Sharing is only supported for boot loader classes because bootstrap classpath has been appended
+			jvmArgs("-Xshare:off")
 			if (javaVersion.isCompatibleWith(JavaVersion.VERSION_21)) {
 				// https://github.com/mockito/mockito/issues/3037#issuecomment-1588199599
 				jvmArgs("-XX:+EnableDynamicAgentLoading")
